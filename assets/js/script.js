6c955bc29811abe4ad8ce277c44aff9a5738b09e
unknown <yelurikesavaraghuveer@gmail.com>
2024-07-16 15:35:33 +0530

    2nd commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-/** 
-Author: Build Rise Shine with Nyros (BRS) 
-Created: 2023 
-Library / Component: Script file
-Description: Letter Type Effect
-(c) Copyright by BRS with Nyros. 
-**/
-
 /* Get Our Elements */
 const containerDiv = document.getElementById("content");
 const resetBtn = document.getElementById("resetBtn");
@@ -13,47 +5,47 @@ const resetBtn = document.getElementById("resetBtn");
 // Default theme
 let chathams_blue = "#1A4B84";
 
-// function that sets the content
+// Function that sets the content
 function setContent() {
   const content = `
-  <div class="container">
-  <div class="row">
-    <h2 data-type data-type-min="100" data-type-max="300">
-     Main Heading
-    </h2>
-    <p class="text-start lh-lg" data-type data-type-min="1" data-type-max="6">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi 
-    labore blanditiis dolores voluptas aut eligendi exercitationem perferendis autem 
-    distinctio dolorum magni a quae, facilis excepturi debitis, corrupti suscipit odio. 
-    Recusandae temporibus nobis quod sunt distinctio! Temporibus architecto amet saepe nostrum 
-    ullam dolor nihil soluta magni natus modi nam quasi alias vero ipsa iusto ea, adipisci iure 
-    libero quae numquam, illum animi assumenda voluptas tempora. 
-    Numquam impedit voluptatum ipsam ducimus.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi 
-    labore blanditiis dolores voluptas aut eligendi exercitationem perferendis autem 
-    distinctio dolorum magni a quae, facilis excepturi debitis, corrupti suscipit odio. 
-    Recusandae temporibus nobis quod sunt distinctio! Temporibus architecto amet saepe nostrum 
-    ullam dolor nihil soluta magni natus modi nam quasi alias vero ipsa iusto ea, adipisci iure 
-    libero quae numquam, illum animi assumenda voluptas tempora. 
-    Numquam impedit voluptatum ipsam ducimus.
-    </p>
-  </div>
-</div>
+    <div class="container">
+      <div class="row">
+        <h2 data-type data-type-min="100" data-type-max="300">
+         Main Heading
+        </h2>
+        <p class="text-start lh-lg" data-type data-type-min="1" data-type-max="6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi 
+        labore blanditiis dolores voluptas aut eligendi exercitationem perferendis autem 
+        distinctio dolorum magni a quae, facilis excepturi debitis, corrupti suscipit odio. 
+        Recusandae temporibus nobis quod sunt distinctio! Temporibus architecto amet saepe nostrum 
+        ullam dolor nihil soluta magni natus modi nam quasi alias vero ipsa iusto ea, adipisci iure 
+        libero quae numquam, illum animi assumenda voluptas tempora. 
+        Numquam impedit voluptatum ipsam ducimus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi 
+        labore blanditiis dolores voluptas aut eligendi exercitationem perferendis autem 
+        distinctio dolorum magni a quae, facilis excepturi debitis, corrupti suscipit odio. 
+        Recusandae temporibus nobis quod sunt distinctio! Temporibus architecto amet saepe nostrum 
+        ullam dolor nihil soluta magni natus modi nam quasi alias vero ipsa iusto ea, adipisci iure 
+        libero quae numquam, illum animi assumenda voluptas tempora. 
+        Numquam impedit voluptatum ipsam ducimus.
+        </p>
+      </div>
+    </div>
   `;
   containerDiv.innerHTML = content;
 }
 
-// delay
+// Delay function
 function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// get random number
+// Function to get random number
 function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 
-// recursion
+// Function for recursion
 function draw(el) {
   let index = 1;
   const text = el.textContent;
@@ -72,21 +64,21 @@ function draw(el) {
   drawLetter();
 }
 
-// restart the effect
+// Function to restart the effect
 function restartTypeEffect(params) {
   containerDiv.innerHTML = "";
   setContent();
   document.querySelectorAll("[data-type]").forEach(draw);
 }
 
-// event listener for restart
+// Event listener for restart button
 resetBtn.addEventListener("click", restartTypeEffect);
 
-// Init content 
+// Initialize content on page load
 setContent();
 document.querySelectorAll("[data-type]").forEach(draw);
 
-// theme
+// Theme function
 function setTheme(theme) {
   document.documentElement.style.setProperty("--primary-color", theme);
   localStorage.setItem("movie-theme", theme);
